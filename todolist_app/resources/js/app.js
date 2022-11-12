8edf3b45744630e4fe4337b5348ea64a035c9dac
shozo1030 <navisohhide92@yahoo.co.jp>
2022-11-12 18:11:34 +0900

    fontawesome変更

--- todolist_app/resources/js/app.js
+++ todolist_app/resources/js/app.js
@@ -3,10 +3,10 @@ require('./bootstrap');
 import { createApp } from 'vue'
 import App from './vue/app'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faPlusSquare, faTrash)
+library.add(fas)
 
 export const eventBus = createApp(App)
 createApp(App)
