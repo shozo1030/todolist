47b7dc3bd9ccd1b0ce506e73fa4a928285c4d6d0
shozo1030 <navisohhide92@yahoo.co.jp>
2022-11-14 18:49:45 +0900

    title変更、パスを絶対パスに変更

--- todolist_app/resources/views/welcome.blade.php
+++ todolist_app/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>TodoList</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -18,6 +18,6 @@
     </div>
 
 </body>
-<script src="{{ mix('js/app.js') }}"></script>
+<script src="{{ url(mix('js/app.js')) }}"></script>
 
 </html>
